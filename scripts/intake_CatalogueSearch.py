670524c6c4a24aef73b555f724f8518e101067fa
LeoBertini <leonardo.bertini@liverpool.ac.uk>
2026-09-17 10:54:54 +0100

    improving code annotations

--- scripts/intake_CatalogueSearch.py
+++ scripts/intake_CatalogueSearch.py
@@ -134,7 +134,7 @@ for oceanvar in variable_ids:
         # rather we'd like to retrieve the attributes from the search across all nodes into a DataFrame ...
         # so we can see what is available and where, and do further filtering without having to download heaps of data unnecessarily.
         info = cat.infos_to_dict(quiet=False)  # dictionary structure containing all the urls, SHA256 hashes to enable serialized download
-        DataFrameSearch = pd.DataFrame.from_dict(info['https'])  # all with monthly piControl and historical salinity and potential temperature
+        DataFrameSearch = pd.DataFrame.from_dict(info['https'])  # all with monthly piControl and historical
 
         # Complete TODO split file name so that columns also display the following facets
         # source_id or aka model used
